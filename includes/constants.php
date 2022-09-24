031cb96c6be24b8db131886631f2b7eea5a01150
Yuto Uogata <uouo.yuto@gmail.com>
2022-09-24 14:14:10 -0400

    Submission

--- includes/constants.php
+++ includes/constants.php
@@ -1,6 +1,6 @@
 <?php
     define("DB_HOST", "127.0.0.1");
-    define("DATABASE", "uogatay_db");
-    define("DB_ADMIN", "uogatay");
-    define("DB_PASSWORD", "AKJ4X@");
+    define("DATABASE", "");
+    define("DB_ADMIN", "");
+    define("DB_PASSWORD", "");
 ?>
